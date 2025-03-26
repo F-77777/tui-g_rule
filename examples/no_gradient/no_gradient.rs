7955c193c3b1a7a48aa023a8b5488e81313b5656
chisommaduabuchi8 <chisommaduabuchi8@gmail.com>
2025-03-26 04:31:33 +0000

    .

--- examples/no_gradient/no_gradient.rs
+++ examples/no_gradient/no_gradient.rs
@@ -1,14 +1,23 @@
 tui_g_rule::gen_example_code!(
-    fn run(terminal: &mut DefaultTerminal) -> io::Result<()> {
+    fn run(
+        terminal: &mut DefaultTerminal,
+    ) -> io::Result<()> {
         loop {
             terminal.draw(|f| {
                 let block = Block::bordered();
-                let rule_vert = Rule::default().horizontal_alignment(Alignment::Left)
-                    .vertical().horizontal_margin(4)
-                    .vertical_margin(1);
-		let rule_hor = Rule::default().horizontal_margin(6);
+                let rule_vert = Rule::from_set(
+                    presets::test_sets::VERTICAL,
+                )
+                .horizontal_alignment(Alignment::Left)
+                .vertical()
+                .horizontal_padding(4)
+                .vertical_padding(1);
+                let rule_hor = Rule::from_set(
+                    presets::test_sets::HORIZONTAL,
+                )
+                .horizontal_padding(6);
                 f.render_widget(rule_hor, f.area());
-		f.render_widget(rule_vert, f.area());
+                f.render_widget(rule_vert, f.area());
                 f.render_widget(block, f.area());
             })?;
             if let Err(e) = handle_events() {
